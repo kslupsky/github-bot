1c484fe94e3716f3e2d707baadbdadeae384732e
Kealy Slupsky <kealy.slupsky@platotech.com>
2024-07-24 15:12:34 -0300

    add bot code

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -1,11 +1,33 @@
 import os
+import aiohttp
 from aiohttp import web
+from gidgethub import routing, sansio
+from gidgethub import aiohttp as gh_aiohttp
 
+router = routing.Router()
 routes = web.RouteTableDef()
 
+@router.register("issues", action="opened")
+async def issue_opened_event(event, gh, *args, **kwargs):
+    url = event.data["issue"]["comments_url"]
+    author = event.data["issue"]["user"]["login"]
+    message = f"Thanks @{author} for reporting this issue! My human will start looking into it right away."
+    await gh.post(url, data={"body": message})
+
 @routes.get("/")
-async def main(request):
+async def index(request):
     return web.Response(status=200, text="Hello World")
+
+@routes.post("/")
+async def main(request):
+    body = await request.read()
+    secret = os.environ.get("GH_SECRET")
+    oauth = os.environ.get("GH_AUTH")
+    event = sansio.Event.from_http(request.headers, body, secret=secret)
+    async with aiohttp.ClientSession() as session:
+        gh = gh_aiohttp.GitHubAPI(session, "kslupsky", oauth_token=oauth)
+        await router.dispatch(event, gh)
+    return web.Response(status=200)
 
 if __name__ == "__main__":
     app = web.Application()
